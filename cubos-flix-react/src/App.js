bed6d7ec4754944ce7607546cba2f42642361a6a
Eloisa Sampaio <eloisa.smedina@gmail.com>
2021-05-12 22:21:03 -0300

    função de busca e props

--- cubos-flix-react/src/App.js
+++ cubos-flix-react/src/App.js
@@ -1,13 +1,32 @@
+import { useState } from "react";
+
 import Header from "./componentes/header";
-import ContainerCards from "./componentes/container";
+import MainContainer from "./componentes/mainContainer";
 import Cart from "./componentes/sacola";
+import movies from "./data";
 import "./App.css";
 
 function App() {
+  const [currentMovie, setCurrentMovie] = useState(undefined);
+
+  const onInputHeaderChange = (e) => {
+    const inputValue = e.target.value;
+
+    if (e.key !== "Enter") return;
+
+    const filmeEncontrado = movies.find((movie) =>
+      movie.title.includes(inputValue)
+    );
+
+    setCurrentMovie(filmeEncontrado);
+  };
+
+  console.log(currentMovie);
+
   return (
     <div className="App">
-      <Header />
-      <ContainerCards />
+      <Header onSearchChange={onInputHeaderChange} />
+      <MainContainer currentMovie={currentMovie} />
       <aside className="App-aside">
         <Cart />
       </aside>
