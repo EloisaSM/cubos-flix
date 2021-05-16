5d457c1c29eafc119d51879b45636dd87b02d247
Eloisa Sampaio <eloisa.smedina@gmail.com>
2021-05-16 18:14:35 -0300

    filtro por categoria

--- cubos-flix-react/src/componentes/filmesCategory.js
+++ cubos-flix-react/src/componentes/filmesCategory.js
@@ -1,23 +1,37 @@
 import React from "react";
+import { useState } from "react";
 
 import ListMovies from "./listMovies";
 import SingleMovieSearch from "./singleMovie";
+import SingleMovieFiltered from "./singleMovieFiltered";
 import FilterBtn from "./filtersBtn";
 
 function FilmesCategory(props) {
   const { movies, currentMovie } = props;
 
+  const [filteredMovies, setFiltereMovies] = useState(movies);
+
+  function onFilterChange(filteredMovies) {
+    if (!filteredMovies.length) {
+      return setFiltereMovies(movies);
+    }
+
+    console.log(filteredMovies);
+
+    setFiltereMovies(filteredMovies);
+  }
+
   return (
     <div className="filmes-container">
       <h2>Filmes</h2>
 
-      <FilterBtn />
+      <FilterBtn movies={movies} onFilter={onFilterChange} />
 
       {currentMovie ? (
         <SingleMovieSearch movie={currentMovie} />
       ) : (
         <div className="filmes-box">
-          <ListMovies movies={movies.slice(0, 7)} />
+          <ListMovies movies={filteredMovies} />
         </div>
       )}
     </div>
