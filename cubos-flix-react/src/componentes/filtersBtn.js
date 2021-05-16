df9c5f8fad87f2f14945b941fd2fc8b406a3e7b2
Eloisa Sampaio <eloisa.smedina@gmail.com>
2021-05-16 18:13:23 -0300

    alteração categorias e funcao chageFilter

--- cubos-flix-react/src/componentes/filtersBtn.js
+++ cubos-flix-react/src/componentes/filtersBtn.js
@@ -5,38 +5,52 @@ const filters = [
   {
     content: "Todos",
     value: "Todos",
+    categoria: "Todos",
   },
   {
     content: "Ação",
     value: "Ação",
+    category: "action",
   },
   {
     content: "Romance",
-    value: "Romance",
+    category: "romance",
   },
   {
     content: "Ficção Cientifica",
     value: "Ficção Cientifica",
+    category: "science fiction",
   },
   {
     content: "Terror",
     value: "Terror",
+    category: "horror",
   },
 ];
 
 const filterDefalut = filters[0];
 
-function FilterBtn() {
-  const [currentFilter, setCurrentFilter] = useState(filterDefalut.value);
+function FilterBtn(props) {
+  const { movies, onFilter } = props;
+  const [selectedFilter, setSelectedFilter] = useState(filterDefalut.value);
+
+  function changeFilter(category, filterContent) {
+    const filteredMovies = movies.filter((movie) =>
+      movie.categories.includes(category)
+    );
+
+    setSelectedFilter(filterContent);
+    onFilter(filteredMovies);
+  }
 
   return (
     <div className="filters">
       {filters.map((filter, idx) => (
         <button
           key={`filters-btn-${idx}`}
-          className={currentFilter === filter.value ? "selecionado" : ""}
+          className={selectedFilter === filter.content ? "selecionado" : ""}
           onClick={() => {
-            setCurrentFilter(filter.value);
+            changeFilter(filter.category, filter.content);
           }}
         >
           {filter.content}
