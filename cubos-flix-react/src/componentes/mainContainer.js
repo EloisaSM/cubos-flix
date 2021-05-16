33646f986383aa177cae58da8f1d93ef30b81fd1
Eloisa Sampaio <eloisa.smedina@gmail.com>
2021-05-16 18:16:49 -0300

    remoção props

--- cubos-flix-react/src/componentes/mainContainer.js
+++ cubos-flix-react/src/componentes/mainContainer.js
@@ -4,16 +4,13 @@ import ContainerMovies from "./containerMovies";
 import BannerCupom from "./bannerCumpom";
 
 function MainContainer(props) {
-  const { currentMovie, onFilterChange } = props;
+  const { currentMovie } = props;
 
   return (
     <main className="App-container-cards">
       <BannerCupom />
 
-      <ContainerMovies
-        currentMovie={currentMovie}
-        onFilterChange={onFilterChange}
-      />
+      <ContainerMovies currentMovie={currentMovie} />
     </main>
   );
 }
